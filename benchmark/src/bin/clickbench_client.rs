01a196b7d35530edc0edc19791b6d8637760662f
WetABQ <wetabq@gmail.com>
2025-02-04 17:56:27 -0600

    better float eq

--- benchmark/src/bin/clickbench_client.rs
+++ benchmark/src/bin/clickbench_client.rs
@@ -7,7 +7,11 @@ use std::{
 };
 
 use datafusion::{
-    arrow::{array::RecordBatch, util::pretty},
+    arrow::{
+        array::{AsArray, RecordBatch},
+        datatypes::{DataType, Float16Type, Float32Type, Float64Type},
+        util::pretty,
+    },
     error::Result,
     parquet::{
         arrow::{ArrowWriter, arrow_reader::ParquetRecordBatchReaderBuilder},
@@ -63,6 +67,21 @@ fn get_query(
     }
 }
 
+macro_rules! float_eq {
+    ($left:expr, $right:expr, $type:ty, $abs_f:expr) => {{
+        use datafusion::arrow::compute::kernels::numeric::sub_wrapping;
+        let diff = sub_wrapping(&$left, &$right).unwrap();
+        let diff = diff.as_primitive_opt::<$type>().unwrap();
+        for d in diff.iter() {
+            if let Some(d) = d {
+                if $abs_f(d) {
+                    return false;
+                }
+            }
+        }
+    }};
+}
+
 fn assert_batch_eq(left: &RecordBatch, right: &RecordBatch) -> bool {
     use datafusion::arrow::compute::*;
 
@@ -77,8 +96,27 @@ fn assert_batch_eq(left: &RecordBatch, right: &RecordBatch) -> bool {
         let sorted_c_l = sort(&casted, None).unwrap();
         let sorted_c_r = sort(c_r, None).unwrap();
 
-        if sorted_c_l != sorted_c_r {
-            return false;
+        let data_type = c_l.data_type();
+        let tol: f32 = 1e-9;
+        match data_type {
+            DataType::Float16 => {
+                use half::f16;
+                let abs_f = |d: f16| d.to_f32().abs() > tol;
+                float_eq!(sorted_c_l, sorted_c_r, Float16Type, abs_f)
+            }
+            DataType::Float32 => {
+                let abs_f = |d: f32| d.abs() > tol;
+                float_eq!(sorted_c_l, sorted_c_r, Float32Type, abs_f)
+            }
+            DataType::Float64 => {
+                let abs_f = |d: f64| d.abs() > tol.into();
+                float_eq!(sorted_c_l, sorted_c_r, Float64Type, abs_f)
+            }
+            _ => {
+                if sorted_c_l != sorted_c_r {
+                    return false;
+                }
+            }
         }
     }
     true
