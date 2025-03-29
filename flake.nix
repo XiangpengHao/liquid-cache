fd84e1fe2d88f91ecaf5081139586791943f01de
Ao Li <aoli.al@hotmail.com>
2025-03-28 23:14:57 -0400

    Add flake.nix for reproducible build.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Liquid Cache Flake Configuration";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+            (rust-bin.fromRustupToolchainFile (./rust-toolchain.toml))
+          ];
+        };
+      }
+    );
+}
