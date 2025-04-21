f3147713522391d4dc1b11354f48d774ea403443
Xiangpeng Hao <me@xiangpeng.systems>
2025-04-21 09:08:00 -0500

    update fsst benchmark

--- src/liquid_parquet/bench/fsstarray.rs
+++ src/liquid_parquet/bench/fsstarray.rs
@@ -5,73 +5,58 @@ use std::time::Duration;
 extern crate arrow;
 
 use arrow::{
-    array::{StringArray, StringBuilder},
+    array::{Array, StringArray, StringBuilder},
     datatypes::Utf8Type,
 };
 use liquid_cache_parquet::liquid_array::raw::FsstArray;
 use std::fs;
 
-const FOLDER_PATH: &str = "./bench/training_texts/";
+const CHUNK_SIZE: [usize; 5] = [12, 32, 64, 128, 256];
 
-// Function to create a vector of StringArray from files in a folder
-fn create_string_arrays_from_files(folder_path: &str) -> Vec<StringArray> {
-    let mut string_arrays = Vec::new();
+fn create_string_arrays_from_file() -> Vec<(usize, StringArray)> {
+    const TEST_FILE_PATH: &str = "../../README.md";
+    const LICENSE_FILE_PATH: &str = "../../LICENSE";
 
-    // Read all entries in the specified folder
-    let entries = fs::read_dir(folder_path).expect("Failed to read directory");
+    let readme = fs::read_to_string(TEST_FILE_PATH).expect("Failed to read file");
+    let license = fs::read_to_string(LICENSE_FILE_PATH).expect("Failed to read file");
+    let content = format!("{}\n\n{}", readme, license);
 
-    // Collect and sort entries by file name
-    let mut entries: Vec<_> = entries.map(|e| e.expect("Failed to read entry")).collect();
-    entries.sort_by_key(|e| e.file_name());
+    let mut result = Vec::new();
 
-    for entry in entries {
-        let path = entry.path();
+    let chars: Vec<char> = content.chars().collect();
 
-        // Skip non-file entries or hidden files (starting with '.')
-        if !path.is_file() || path.file_name().unwrap().to_str().unwrap().starts_with('.') {
-            continue;
-        }
-
-        let content = fs::read_to_string(&path).expect("Failed to read file");
-
+    for &chunk_size in &CHUNK_SIZE {
         let mut builder = StringBuilder::new();
-
-        builder.append_value(&content);
-
-        string_arrays.push(builder.finish());
+        for chunk in chars.chunks(chunk_size) {
+            let chunk_str: String = chunk.iter().collect();
+            builder.append_value(chunk_str);
+        }
+        result.push((chunk_size, builder.finish()));
     }
 
-    string_arrays
+    result
 }
 
 // Benchmark for training the FSST compressor
 fn compressor_benchmark(c: &mut Criterion) {
-    let string_arrays = create_string_arrays_from_files(FOLDER_PATH);
+    let string_arrays = create_string_arrays_from_file();
 
-    let mut group = c.benchmark_group("fsst_array_benchmark");
-    for i in 0..string_arrays.len() {
+    let mut group = c.benchmark_group("fsst");
+    for (chunk_size, string_array) in string_arrays {
+        let total_size = chunk_size * string_array.len();
         // Set the measurement time for the benchmark
         group.measurement_time(Duration::new(10, 0));
 
-        // Calculate the total byte size of the training text
-        let string_byte_size: usize = string_arrays[i]
-            .iter()
-            .filter_map(|s| s) // Skip null values
-            .map(|s| s.len()) // Get the byte length of each string
-            .sum();
-
         // Set the throughput for the benchmark
-        group.throughput(Throughput::Bytes(string_byte_size as u64));
+        group.throughput(Throughput::Bytes(total_size as u64));
 
         // Benchmark the FSST compressor training
         group.bench_function(
-            format!("train_compressor - str_size: {}", string_byte_size),
+            format!("train_compressor - chunk_size: {}", chunk_size),
             |b| {
                 b.iter(|| {
                     let input = criterion::black_box(
-                        string_arrays[i]
-                            .iter()
-                            .flat_map(|s| s.map(|a| a.as_bytes())),
+                        string_array.iter().flat_map(|s| s.map(|a| a.as_bytes())),
                     );
                     FsstArray::train_compressor(input)
                 });
@@ -83,80 +68,64 @@ fn compressor_benchmark(c: &mut Criterion) {
 
 // Benchmark for creating an FSST array from a byte array using a pre-trained compressor
 fn from_byte_array_with_compressor_benchmark(c: &mut Criterion) {
-    let string_arrays = create_string_arrays_from_files(FOLDER_PATH);
+    let string_arrays = create_string_arrays_from_file();
 
-    let mut group = c.benchmark_group(format!("compressor_benchmark"));
-    for i in 0..string_arrays.len() {
+    let mut group = c.benchmark_group(format!("fsst"));
+    for (chunk_size, string_array) in string_arrays {
         // Train the FSST compressor
-        let compressor = FsstArray::train_compressor(
-            string_arrays[i]
-                .iter()
-                .flat_map(|s| s.map(|s| s.as_bytes())),
-        );
+        let compressor =
+            FsstArray::train_compressor(string_array.iter().flat_map(|s| s.map(|s| s.as_bytes())));
 
-        // Calculate the total byte size of the training text
-        let string_byte_size: usize = string_arrays[i]
-            .iter()
-            .filter_map(|s| s) // Skip null values
-            .map(|s| s.len()) // Get the byte length of each string
-            .sum();
+        let compressed =
+            FsstArray::from_byte_array_with_compressor(&string_array, Arc::new(compressor.clone()));
+        let compressed_size = compressed.get_array_memory_size();
+        let uncompressed_size = chunk_size * string_array.len();
+        println!(
+            "compressed_size: {}, uncompressed_size: {}, compression_ratio: {}",
+            compressed_size,
+            uncompressed_size,
+            compressed_size as f64 / uncompressed_size as f64
+        );
 
         // Set the throughput for the benchmark
-        group.throughput(Throughput::Bytes(string_byte_size as u64));
+        group.throughput(Throughput::Bytes(uncompressed_size as u64));
 
         // Benchmark the creation of an FSST array from a byte array
-        group.bench_function(
-            format!(
-                "from_byte_array_with_compressor - str_size: {}",
-                string_byte_size
-            ),
-            |b| {
-                b.iter(|| {
-                    criterion::black_box(FsstArray::from_byte_array_with_compressor(
-                        &string_arrays[i],
-                        Arc::new(compressor.clone()),
-                    ))
-                });
-            },
-        );
+        group.bench_function(format!("compress - chunk_size: {}", chunk_size), |b| {
+            b.iter(|| {
+                criterion::black_box(FsstArray::from_byte_array_with_compressor(
+                    &string_array,
+                    Arc::new(compressor.clone()),
+                ))
+            });
+        });
     }
     group.finish();
 }
 
 // Benchmark for converting an FSST array to an Arrow byte array
 fn to_arrow_byte_array_benchmark(c: &mut Criterion) {
-    let string_arrays = create_string_arrays_from_files(FOLDER_PATH);
+    let string_arrays = create_string_arrays_from_file();
 
-    let mut group = c.benchmark_group(format!("compressor_benchmark"));
-    for i in 0..string_arrays.len() {
+    let mut group = c.benchmark_group(format!("fsst"));
+    for (chunk_size, string_array) in string_arrays {
         // Train the FSST compressor
-        let compressor = FsstArray::train_compressor(
-            string_arrays[i]
-                .iter()
-                .flat_map(|s| s.map(|s| s.as_bytes())),
-        );
+        let compressor =
+            FsstArray::train_compressor(string_array.iter().flat_map(|s| s.map(|s| s.as_bytes())));
 
         // Create an FSST array using the trained compressor
         let fsst_values =
-            FsstArray::from_byte_array_with_compressor(&string_arrays[i], Arc::new(compressor));
+            FsstArray::from_byte_array_with_compressor(&string_array, Arc::new(compressor));
 
-        // Calculate the total byte size of the training text
-        let string_byte_size: usize = string_arrays[i]
-            .iter()
-            .filter_map(|s| s) // Skip null values
-            .map(|s| s.len()) // Get the byte length of each string
-            .sum();
+        let total_size = chunk_size * string_array.len();
 
         // Set the throughput for the benchmark
-        group.throughput(Throughput::Bytes(string_byte_size as u64));
+        group.throughput(Throughput::Bytes(total_size as u64));
 
         // Benchmark the conversion of FSST array to Arrow byte array
-        group.bench_function(
-            format!("to_arrow_byte_array - str_size: {}", string_byte_size),
-            |b| {
-                b.iter(|| criterion::black_box(fsst_values.to_arrow_byte_array::<Utf8Type>()));
-            },
-        );
+        group.bench_function(format!("decompress - chunk_size: {}", chunk_size), |b| {
+            b.iter(|| criterion::black_box(fsst_values.to_arrow_byte_array::<Utf8Type>()));
+        });
     }
     group.finish();
 }
