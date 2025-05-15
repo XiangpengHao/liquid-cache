caa05f64d051df93c255a2b37f68f56c099e5065
Devan <devandbenz@gmail.com>
2025-05-15 09:00:20 -0500

    feat: working on it

--- src/server/src/admin_server/task_monitor.rs
+++ src/server/src/admin_server/task_monitor.rs
@@ -49,45 +49,58 @@ impl LiquidTaskMonitor {
         let monitor_interval = self.monitor.intervals();
         let rt_monitor_interval = self.rt_monitor.intervals();
 
-        let cancellation_token = self.cancellation_token.clone();
+        let cancellation_token_mon = self.cancellation_token.clone();
+        let cancellation_token_rt = self.cancellation_token.clone();
         let liquid_task_metrics = self.liquid_task_metrics.clone();
+        let liquid_task_metrics_rt = self.liquid_task_metrics.clone();
 
-        tokio::spawn(async move {
-            fn update_high_mark(counter: &AtomicU64, new_value: u64) {
-                let mut curr = counter.load(Ordering::Relaxed);
-                loop {
-                    if new_value <= curr {
-                        break;
-                    }
+        fn update_high_mark(counter: &AtomicU64, new_value: u64) {
+            let mut curr = counter.load(Ordering::Relaxed);
+            loop {
+                if new_value <= curr {
+                    break;
+                }
 
-                    match counter.compare_exchange(curr, new_value, Ordering::Relaxed, Ordering::Relaxed) {
-                        Ok(_) => break,
-                        Err(actual) => curr = actual,
-                    }
+                match counter.compare_exchange(
+                    curr,
+                    new_value,
+                    Ordering::Relaxed,
+                    Ordering::Relaxed,
+                ) {
+                    Ok(_) => break,
+                    Err(actual) => curr = actual,
                 }
             }
+        }
 
-            let mut rt_stream = Box::pin(rt_monitor_interval);
-            let mut metrics_stream = Box::pin(monitor_interval);
-
-            loop {
-                if cancellation_token.load(Ordering::Acquire) {
+        tokio::spawn(async move {
+            for metrics in monitor_interval {
+                if cancellation_token_mon.load(Ordering::Acquire) {
                     return;
                 }
 
-                tokio::select! {
-                Some(rt_metrics) = rt_stream.next() => {
-                    update_high_mark(&liquid_task_metrics.total_tasks_n, rt_metrics.live_tasks_count as u64);
-                }
+                update_high_mark(
+                    &liquid_task_metrics.total_slow_poll_n,
+                    metrics.total_slow_poll_count,
+                );
+                update_high_mark(&liquid_task_metrics.total_poll_n, metrics.total_poll_count);
 
-                Some(metrics) = metrics_stream.next() => {
-                    update_high_mark(&liquid_task_metrics.total_slow_poll_n, metrics.total_slow_poll_count);
-                    update_high_mark(&liquid_task_metrics.total_poll_n, metrics.total_poll_count);
-                }
-
-                _ = tokio::time::sleep(tokio::time::Duration::from_millis(500)) => {
-                }
+                _ = tokio::time::sleep(tokio::time::Duration::from_millis(500));
             }
+        });
+
+        tokio::spawn(async move {
+            for metrics in rt_monitor_interval {
+                if cancellation_token_rt.load(Ordering::Acquire) {
+                    return;
+                }
+
+                update_high_mark(
+                    &liquid_task_metrics_rt.total_tasks_n,
+                    metrics.live_tasks_count as u64,
+                );
+
+                _ = tokio::time::sleep(tokio::time::Duration::from_millis(500));
             }
         });
     }
@@ -98,18 +111,35 @@ impl LiquidTaskMonitor {
 
     pub async fn get_metrics(&self) -> LiquidTaskMetricsResponse {
         LiquidTaskMetricsResponse {
-            total_tasks_n: self.liquid_task_metrics.total_tasks_n.load(Ordering::Acquire),
-            total_slow_poll_n: self.liquid_task_metrics.total_slow_poll_n.load(Ordering::Acquire),
-            total_poll_n: self.liquid_task_metrics.total_poll_n.load(Ordering::Acquire),
+            total_tasks_n: self
+                .liquid_task_metrics
+                .total_tasks_n
+                .load(Ordering::Acquire),
+            total_slow_poll_n: self
+                .liquid_task_metrics
+                .total_slow_poll_n
+                .load(Ordering::Acquire),
+            total_poll_n: self
+                .liquid_task_metrics
+                .total_poll_n
+                .load(Ordering::Acquire),
             io_usage_n: self.liquid_task_metrics.io_usage_n.load(Ordering::Acquire),
         }
     }
 
     pub fn reset_counters(&self) {
-        self.liquid_task_metrics.total_slow_poll_n.store(0, Ordering::Relaxed);
-        self.liquid_task_metrics.total_poll_n.store(0, Ordering::Relaxed);
-        self.liquid_task_metrics.io_usage_n.store(0, Ordering::Relaxed);
-        self.liquid_task_metrics.total_tasks_n.store(0, Ordering::Relaxed);
+        self.liquid_task_metrics
+            .total_slow_poll_n
+            .store(0, Ordering::Relaxed);
+        self.liquid_task_metrics
+            .total_poll_n
+            .store(0, Ordering::Relaxed);
+        self.liquid_task_metrics
+            .io_usage_n
+            .store(0, Ordering::Relaxed);
+        self.liquid_task_metrics
+            .total_tasks_n
+            .store(0, Ordering::Relaxed);
         self.cancellation_token.store(false, Ordering::Relaxed);
     }
 }
