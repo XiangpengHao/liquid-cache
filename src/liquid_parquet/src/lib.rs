2860a6a90b1769d1c31a47ec2c25f64cab908246
Xiangpeng Hao <me@xiangpeng.systems>
2025-07-12 17:40:47 -0500

    update

--- src/liquid_parquet/src/lib.rs
+++ src/liquid_parquet/src/lib.rs
@@ -16,5 +16,3 @@ pub use inprocess::LiquidCacheInProcessBuilder;
 pub use liquid_cache_common as common;
 pub use reader::{FilterCandidate, FilterCandidateBuilder, LiquidParquetSource, LiquidPredicate};
 pub use utils::{boolean_buffer_and_then, extract_execution_metrics, rewrite_data_source_plan};
-
-
