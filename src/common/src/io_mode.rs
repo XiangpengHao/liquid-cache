e077e6684cfb62d61a8911e2eac97486025c0f67
XiangpengHao <me@xiangpeng.systems>
2025-10-29 22:42:50 -0500

    make it fast again

--- src/common/src/io_mode.rs
+++ src/common/src/io_mode.rs
@@ -14,13 +14,13 @@ pub enum IoMode {
     Uring,
 
     /// Uses io_uring on the calling thread and blocks until completion.
+    #[default]
     #[serde(rename = "uring-blocking")]
     UringBlocking,
 
     /// Uses rust's std::fs::File, this is blocking IO.
     /// On Linux, this is essentially `pread/pwrite`
     /// This is the default until we optimized the performance of uring.
-    #[default]
     #[serde(rename = "std-blocking")]
     StdBlocking,
 
